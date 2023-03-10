4171eda6c17364402fcc9f7148ef538b371066cf
Justin9606 <tonybek.96@gmail.com>
2023-03-10 14:34:20 +0900

    changed to 1

--- src/Roulette/Roulette.js
+++ src/Roulette/Roulette.js
@@ -2,24 +2,24 @@ const Roulette = ({
   reset_callback = () => {},
   start_callback = () => {},
   has_reset = true,
-  start_text = 'Start!',
-  reset_text = 'Reset',
+  start_text = "Sta!",
+  reset_text = "Reset",
   ...props
 }) => {
-  const [start, set_start] = useState(false)
-  const [show_reset, set_show_reset] = useState(false)
-  const [forbid_start, set_forbid_start] = useState(false)
+  const [start, set_start] = useState(false);
+  const [show_reset, set_show_reset] = useState(false);
+  const [forbid_start, set_forbid_start] = useState(false);
 
   const clickToReset = () => {
-    set_start(false)
-    reset_callback()
-    set_show_reset(false)
-    set_forbid_start(true)
-  }
+    set_start(false);
+    reset_callback();
+    set_show_reset(false);
+    set_forbid_start(true);
+  };
   const clickToTrigger = () => {
-    set_start(true)
-    start_callback()
-  }
+    set_start(true);
+    start_callback();
+  };
 
   return (
     <RouletteContainer className="roulette-container">
@@ -37,19 +37,14 @@ const Roulette = ({
         {start_text}
       </RoulleteBtn>
 
-      {(has_reset && show_reset) && (
-        <RoulleteBtn
-          onClick={clickToReset}
-          className="reset-btn"
-        >
+      {has_reset && show_reset && (
+        <RoulleteBtn onClick={clickToReset} className="reset-btn">
           {reset_text}
         </RoulleteBtn>
       )}
     </RouletteContainer>
-  )
-}
-
-
+  );
+};
 
 const RouletteBody = ({
   roulette_img_under_highlight,
@@ -60,12 +55,12 @@ const RouletteBody = ({
   prize_arr,
   set_show_reset,
   set_forbid_start,
-  on_complete = () => {}
+  on_complete = () => {},
 }) => {
-  const UBoardRef = useRef(null)
-  const OBoardRef = useRef(null)
-  const HighlightRef = useRef(null)
-  const prize_num = generate_random_number(0, prize_arr.length - 1)
+  const UBoardRef = useRef(null);
+  const OBoardRef = useRef(null);
+  const HighlightRef = useRef(null);
+  const prize_num = 1;
 
   const trigger_roulette = () => {
     TweenLite.to([UBoardRef.current, OBoardRef.current], 3, {
@@ -79,42 +74,40 @@ const RouletteBody = ({
             TweenMax.to(HighlightRef.current, 0.5, {
               alpha: 1,
               yoyo: true,
-              repeat: -1
-            })
-            set_show_reset(true)
-            on_complete(prize_arr[prize_num])
-          }
-        })
-      }
-    })
-  }
+              repeat: -1,
+            });
+            set_show_reset(true);
+            on_complete(prize_arr[prize_num]);
+          },
+        });
+      },
+    });
+  };
 
   const reset_roulette = () => {
     TweenLite.to([UBoardRef.current, OBoardRef.current], 1, {
       rotation: 0,
       ease: Power2.easeIn,
       onComplete: () => {
-        set_forbid_start(false)
-      }
-    })
+        set_forbid_start(false);
+      },
+    });
     TweenMax.to(HighlightRef.current, 0.5, {
       opacity: 0,
-    })
-  }
+    });
+  };
 
   useEffect(() => {
     if (start) {
-      trigger_roulette()
+      trigger_roulette();
     } else {
-      reset_roulette()
+      reset_roulette();
     }
-  }, [start])
+  }, [start]);
 
   return (
     <ThemeProvider theme={Object.assign({}, general_theme)}>
-      <RouletteBox
-        className="Roulette-box"
-      >
+      <RouletteBox className="Roulette-box">
         <RouletteBoard
           ref={UBoardRef}
           Roulette_img={roulette_img_under_highlight}
@@ -133,19 +126,13 @@ const RouletteBody = ({
         <Pointer pointer_img={pointer_img} className="pointer" />
       </RouletteBox>
     </ThemeProvider>
-  )
-}
+  );
+};
 
+const generate_random_number = (start = 1, end = 10) =>
+  Math.floor(Math.random() * end) + start;
 
-
-
-
-const generate_random_number = (start = 1, end = 10) => (
-  Math.floor(Math.random() * end) + start
-)
-
-
-import React, { useRef, useEffect, useState } from "react"
+import React, { useRef, useEffect, useState } from "react";
 import {
   RouletteBox,
   RouletteBoard,
@@ -153,8 +140,8 @@ import {
   Pointer,
   RoulleteBtn,
   RouletteContainer,
-  general_theme
-} from "./styleComp"
-import { TweenLite, TweenMax, Power2 } from 'gsap'
-import { ThemeProvider } from 'styled-components'
-export default Roulette
+  general_theme,
+} from "./styleComp";
+import { TweenLite, TweenMax, Power2 } from "gsap";
+import { ThemeProvider } from "styled-components";
+export default Roulette;
